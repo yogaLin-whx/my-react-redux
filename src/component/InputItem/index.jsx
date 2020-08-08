afe77d02b2a507d2903405073f688edd58235d6f
yogaLin-whx <whx13058048980>
2020-08-09 07:58:35 +0800

    debug:input cleat the text when add a item

--- src/component/InputItem/index.jsx
+++ src/component/InputItem/index.jsx
@@ -6,24 +6,27 @@ class InputItem extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            inputValue: ""
+            inputValue: ''
         }
     }
 
     handleChange = (e) => {
         this.setState({
             inputValue: e.target.value
-        })
+        });
     }
 
     handleAddItem = () => {
         this.props.handleAddItem(this.state.inputValue);
+        this.setState({
+            inputValue: ''
+        })
     }
 
     render() {
         return (
             <>
-                <input placeholder="inset todo item" onChange={this.handleChange.bind(this)} /><button onClick={this.handleAddItem}>Add</button>
+                <input placeholder="inset todo item" onChange={this.handleChange.bind(this)} value={this.state.inputValue}/><button onClick={this.handleAddItem}>Add</button>
             </>
         )
     }
